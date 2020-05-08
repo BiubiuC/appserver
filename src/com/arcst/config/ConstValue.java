00bcd053293709363485c4f5a7f6517f92fc79cb
BiubiuC <418334237@qq.com>
2020-05-08 15:28:47 +0800

    sort values

--- src/com/arcst/config/ConstValue.java
+++ src/com/arcst/config/ConstValue.java
@@ -2,16 +2,16 @@ package com.arcst.config;
 
 public class ConstValue {
 
-	public static final boolean IS_USED_DB = false;
+	
 	public static final Object CURR_DISPATCHER = "__CURR_DISPATCHER__";
 	public static final Object MAIN_NETACCEPTER = "__MAIN_NETACCEPTER__";
 	
 	
 	
-	
-	public static boolean DEF_DISPATCHER;
-	public static boolean DEF_NETACCEPTER;
-	public static String DISPATCH_CLASS;
-	public static String NETACCEPTER_CLASS;
+	public static boolean IS_USED_DB = false;
+	public static boolean DEF_DISPATCHER = false;
+	public static boolean DEF_NETACCEPTER = false;
+	public static String DISPATCH_CLASS = null;
+	public static String NETACCEPTER_CLASS = null;
 	
 }
