9db122cc59a4eae1b964c2b9d33f862bcef59f49
BiubiuC <418334237@qq.com>
2020-05-08 17:08:05 +0800

    commit test2

---
+++ src/com/arcst/test/ReadConfFile.java
@@ -0,0 +1,8 @@
+package com.arcst.test;
+
+public class ReadConfFile {
+	
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("com.arcst.config.ConfigLoader");
+	} 
+}
